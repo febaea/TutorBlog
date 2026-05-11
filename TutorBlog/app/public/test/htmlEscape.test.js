ecf0312ca771adf9d283f0cdae155d2b673ed4ab
Lucas <lucaspage2005@gmail.com>
2026-05-11 23:13:21 +0100

    update test

--- TutorBlog/app/public/test/htmlEscape.test.js
+++ TutorBlog/app/public/test/htmlEscape.test.js
@@ -1,16 +1,23 @@
 import { htmlEscape } from "../js/htmlEscape.js";
 import assert from "assert";
 
+//Tests for htmlEscape function
+// Starts by setting the variables then runs them by the function and checks that the output is correct
 describe("htmlEscape tests", function () {
-  it("should escape special characters in a string", () => {
-    const input = '<script>alert("XSS")</script>';
-    const expected = "&lt;script&gt;alert(&quot;XSS&quot;)&lt;/script&gt;";
-    const result = htmlEscape(input);
-    assert.strictEqual(result, expected);
-  });
   it("should return the same string if there are no special characters", () => {
     const input = "Hello, World!";
     const expected = "Hello, World!";
+    const result = htmlEscape(input);
+    assert.strictEqual(result, expected);
+  });
+  it("input containing special characters should be changed by the function", () => {
+    const input = '<script>alert("XSS")</script>';
+    const result = htmlEscape(input);
+    assert.notEqual(result, input);
+  });
+  it("should escape special characters in a string to their ascii code", () => {
+    const input = '<script>alert("XSS")</script>';
+    const expected = "&lt;script&gt;alert(&quot;XSS&quot;)&lt;/script&gt;";
     const result = htmlEscape(input);
     assert.strictEqual(result, expected);
   });
